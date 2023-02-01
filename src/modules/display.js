256bdad4fa5bcbef957156ef3ef1c4649c8458a0
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-02-02 02:11:48 +0300

    Add API functionalities

---
+++ src/modules/display.js
@@ -0,0 +1,17 @@
+import fetchData from "./fetchData";
+const scoreList = document.querySelector('.score-list');
+const displayLists = async () => {
+    const data = await fetchData();
+    const lists = data.result;
+    lists.forEach((item) => {
+        const div = document. createElement('div');
+        div.classList.add('score-List');
+        div.innerHTML = `
+        <p>${item.user}: ${item.score}</p>`;
+        scoreList.appendChild(div);
+    }); 
+};
+
+
+
+export default displayLists;
