71c82a66daa3f5bc5becea174cbe15ae005b34f3
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-02-02 02:14:03 +0300

    Solve linter errors

--- src/modules/fetchData.js
+++ src/modules/fetchData.js
@@ -1,11 +1,11 @@
 const gameId = 'eU8fUe323J6Z3a6jqAvT';
 
 const fetchData = async () => {
-    const data = await fetch(
-        `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`,
-    );
-    const container = await data.json()
-    return container
+  const data = await fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`,
+  );
+  const container = await data.json();
+  return container;
 };
 
 export default fetchData;
